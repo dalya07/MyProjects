1a145c076234f0846602e04890d7f362e8ddbc32
Mansour <151526097+dalya07@users.noreply.github.com>
2024-01-08 23:47:44 +0100

    Add files via upload

---
+++ Pong_Game/paddle.py
@@ -0,0 +1,21 @@
+from turtle import Turtle
+
+class Paddle(Turtle):
+
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        # The square is (20 X 20) so we must divide the size of the shape by 20.
+        # e.g.            100:20 = 5      20:20 = 1
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(position)
+
+    def up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
