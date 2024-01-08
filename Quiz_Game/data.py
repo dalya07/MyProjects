3a97511474313f4fc1b2d3c00624544d63166953
Mansour <151526097+dalya07@users.noreply.github.com>
2024-01-08 23:42:51 +0100

    Add files via upload

---
+++ Quiz_Game/data.py
@@ -0,0 +1,10 @@
+question_data = [
+    {"text": "Rabbits can see what's behind themselves without turning their heads.",
+     "answer": "True"},
+    {"text": "The Axolotl is an amphibian that can spend its whole life in a larval state.",
+     "answer": "True"},
+    {"text": "A flock of crows is known as a homicide.",
+     "answer": "False"},
+    {"text": "Rabbits are carnivores.", "answer": "False"},
+    {"text": "A bear does NOT defecate during hibernation. ", "answer": "True"}
+]
