f267802fe81931ba2539696b52b412197904c642
Mansour <151526097+dalya07@users.noreply.github.com>
2024-01-08 23:32:22 +0100

    Add files via upload

---
+++ CaeserCipher.py
@@ -0,0 +1,56 @@
+print("""           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+                          88                                    
+           88             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+""")
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+            's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+            'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+shift = shift % 26
+
+# Creat the Function
+def ceaser(start_text, shift_amount, cipher_direction):
+    # انشاء سلسلة اي string
+    end_text = ""
+    if cipher_direction == "decode":
+        # 5 * -1 = -5
+        shift_amount *= -1
+    # Each of the char in the start_text
+    for char in start_text:
+        if char in alphabet:
+            # Um den Index des Buchstabens herauszufinden, also so zusagen die stelle
+            position = alphabet.index(char)
+            # Die position des Buchstabens + das shift_amount
+            new_position = position + shift_amount
+            # end_text = end_text + alphabet[new_position]
+            end_text += alphabet[new_position]
+        else:
+            # end_text = end_text + char
+            end_text += char
+    print(f"The {cipher_direction}d text is: {end_text}")
+
+# Call the Function
+ceaser(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+
+
+
+
+
+
